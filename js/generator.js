48331f7519ea2de982ee5d5c5cc706bb2d623f8f
rowana16 <aaron.rowan@gmail.com>
2016-08-08 23:27:03 -0400

    Pagination

--- js/generator.js
+++ js/generator.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+	$("#businessType span").text($('h1').text());
 // Topic Select => Form Entry
 	$(".dropdown-menu")
 		.on("mouseenter","li",function(){
@@ -8,22 +9,46 @@ $(document).ready(function(){
 
 		.on("mouseleave","li",function(){
 			$(this).toggleClass("hover")       
-		})
-
-		.on("click","#selectRestaurant",function(){
-			$('#restaurant').css("display","block");
-		})
-
-		.on("click","li",function(){
-			var transfer = $(this).attr('value');
-			$("#businessType span").text(transfer);	
 		});
+
+		// .on("click","#selectRestaurant",function(){
+		// 	$('#restaurant').css("display","block");
+		// });
+
+		// .on("click","li",function(){
+		// 	var transfer = $(this).attr('value');
+		// 	$("#businessType span").text(transfer);	
+		//});
 
 	$(".solo")
 	.on('click','button', function(){
 		var transfer = $(this).attr('value');
 		$("#businessType span").text(transfer);			
 	});
+
+	var automotive =["AutoBodyShop", "AutoDealer", "AutoPartsStore", "AutoRental", "AutoRepair", "AutoWash", "GasStation", "MotorcycleDealer", "MotorcycleRepair"]
+	var emergencyService =["FireStation", "Hospital", "PoliceStation"]
+	var entertainment =["AmusementPark", "ArtGallery", "Casino", "ComedyClub", "MovieTheater", "NightClub"]
+	var financialServices =["AccountingService", "AutomatedTeller", "BankOrCreditUnion", "InsuranceAgency"]
+	var restaurantFoodservice=["Bakery", "BarOrPub", "Brewery", "CafeOrCoffeeShop", "FastFoodRestaurant", "IceCreamShop", "Restaurant", "Winery"]
+	var healthBeauty =["BeautySalon", "DaySpa", "HairSalon", "HealthClub", "NailSalon", "TattooParlor"]
+	var homeConstruction =["Electrician", "GeneralContractor", "HVACBusiness", "HousePainter", "Locksmith", "MovingCompany", "Plumber", "RoofingContractor"]
+	var lodging =["BedAndBreakfast", "Hostel", "Hotel", "Motel"]
+	var sports =["BowlingAlley", "ExerciseGym", "GolfCourse", "HealthClub", "PublicSwimmingPool", "SkiResort", "SportsClub", "StadiumOrArena", "TennisComplex"]
+	var store =["AutoPartsStore", "BikeStore", "BookStore", "ClothingStore", "ComputerStore", "ConvenienceStore", "DepartmentStore", "ElectronicsStore", "Florist", "FurnitureStore", "GardenStore", "GroceryStore", "HardwareStore", "HobbyShop", "HomeGoodsStore", "JewelryStore", "LiquorStore", "MensClothingStore", "MobilePhoneStore", "MovieRentalStore", "MusicStore", "OfficeEquipmentStore", "OutletStore", "PawnShop", "PetStore", "ShoeStore", "SportingGoodsStore", "TireShop", "ToyStore", "WholesaleStore"]
+	
+	createListItems(automotive,"automotive");
+	createListItems(emergencyService,"emergencyService");
+	createListItems(entertainment,"entertainment");
+	createListItems(financialServices,"financialServices");
+	createListItems(restaurantFoodservice,"restaurantFoodservice");
+	createListItems(healthBeauty,"healthBeauty");
+	createListItems(homeConstruction,"homeConstruction");
+	createListItems(lodging,"lodging");
+	createListItems(sports,"sports");
+	createListItems(store,"store");
+
+
 
 /*	$("#selectRestaurant")
 	.on('click','button',function(){
@@ -47,6 +72,15 @@ $(document).ready(function(){
 		var reservation = $("#reservation").hasClass('active');
 		var menu = $("#menuAddress").val();
 		var cuisine = $('#cuisine').val();
+		var hours = "";
+		if($('#hours1').val() != "") { hours = ('"' + $('#hours1').val() + '"' );}
+		if($('#hours2').val() != "") { hours = hours + ', "' + $('#hours2').val() + '"';}
+		if($('#hours3').val() != "") { hours = hours + ', "' + $('#hours3').val() + '"';}
+		if($('#hours4').val() != "") { hours = hours + ', "' + $('#hours4').val() + '"';}
+		if($('#hours5').val() != "") { hours = hours + ', "' + $('#hours5').val() + '"';}
+		if($('#hours6').val() != "") { hours = hours + ', "' + $('#hours6').val() + '"';} 
+		if($('#hours7').val() != "") { hours = hours + ', "' + $('#hours7').val() + '"';} 
+
 		var payment = "";
 		if ($("#visa").hasClass('active')) {payment = "Visa, ";}
 		if ($("#masterCard").hasClass('active')) {payment += "Master Card, ";}
@@ -60,26 +94,36 @@ $(document).ready(function(){
 		var outputScript = '&lt;script type="application/ld+json"> { <br>';
 		outputScript += '"@context" : "http://schema.org",<br>';
 		outputScript +='"@type" : "' + businessType + '", <br>';
-		outputScript +='"address" : {<br>';
-		outputScript +='   "@type": "PostalAddress",<br>';
-		outputScript +='   "addressLocality": "' + city + '", <br>';
-		outputScript +='   "addressRegion": "' + state + '", <br>';
-		outputScript +='   "postalCode": "' + zip + '", <br>';
-		outputScript +='   "streetAddress": "' + street + '" }, <br>';
-		outputScript +='"name":"' + busName + '",<br>';
-		outputScript +='"url":"' + url + '",<br>';
-		outputScript +='"email":"' + email + '",<br>';
-		outputScript +='"telephone":"' + phoneNum + '",<br>';
-		outputScript +='"aggregateRating":{<br>   "@type":"AggregateRating",<br>'
-		outputScript +='   "ratingValue":"' + rating + '",<br>';
-		outputScript +='   "reviewCount":"' + reviews + '"},<br>';
-		outputScript +='"priceRange":"' + priceRange + '",<br>';
-		if(businessType === 'Restaurant'){
+		if(city != "" && state != "" && zip != "" && street != "")
+		{
+			outputScript +='"address" : {<br>';
+			outputScript +='   "@type": "PostalAddress",<br>';
+			outputScript +='   "addressLocality": "' + city + '", <br>';
+			outputScript +='   "addressRegion": "' + state + '", <br>';
+			outputScript +='   "postalCode": "' + zip + '", <br>';
+			outputScript +='   "streetAddress": "' + street + '" }, <br>';
+		}		
+		if(busName != ""){ outputScript +='"name":"' + busName + '",<br>';}
+		if(url != "") {outputScript +='"url":"' + url + '",<br>';}
+		if(email != "") {outputScript +='"email":"' + email + '",<br>';}
+		if(phoneNum != "") {outputScript +='"telephone":"' + phoneNum + '",<br>';}
+		if(hours != "") {outputScript += '"openingHours": [ <br> ' + hours +'], <br>'}
+		if( rating != "" && reviews != "")
+		{
+			outputScript +='"aggregateRating":{<br>   "@type":"AggregateRating",<br>'
+			outputScript +='   "ratingValue":"' + rating + '",<br>';
+			outputScript +='   "reviewCount":"' + reviews + '"},<br>';
+		}		
+		if(priceRange !="") {outputScript +='"priceRange":"' + priceRange + '",<br>';}		
+		if(businessType === 'Restaurant')
+		{
 			outputScript +='"servesCuisine": ["' + cuisine + '"],'
 			outputScript +='"acceptsReservations":"' + reservation +'",<br>';
 			outputScript +='"menu":"' + menu + '",<br>';
-		}
-		outputScript +='"paymentAccepted":"' + payment + '"<br>';
+		}		
+		if(payment !="" ) {outputScript +='"paymentAccepted":"' + payment + '"<br>';}
+
+		if(outputScript.substring(outputScript.length-5,outputScript.length-4)===","){outputScript=outputScript.substring(0,outputScript.length-5) + "<br>";}
 		outputScript += '} &lt;/script>'
 
 		$("#output").html(outputScript);
@@ -88,3 +132,32 @@ $(document).ready(function(){
 
 });
 
+function createListItems (array, destination){
+	var pasteString = ""
+
+	for (var i = 0; i<array.length; i++){
+		pasteString += '<li><a href= "/MicrodataGenerator/' + array[i] + '.html">' + array[i] + '</a></li>';
+	}
+
+	document.getElementById(destination).innerHTML = pasteString;
+}
+
+// <!DOCTYPE html>
+// <html>
+// <body>
+
+// <p id="demo"></p>
+
+// <script>
+// var cars = ["BMW", "Volvo", "Saab", "Ford"];
+// var text = "";
+// var i;
+// for (i = 0; i < cars.length; i++) {
+//     text += cars[i] + "<br>";
+// }
+
+// document.getElementById("demo").innerHTML = text;
+// </script>
+
+// </body>
+// </html>
